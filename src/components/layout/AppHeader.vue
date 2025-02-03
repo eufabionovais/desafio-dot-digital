235dbd5d153335afafd431aec5433bf7fdaaaadc
Fábio Novais <fabio.novais@gmail.com>
2025-02-03 11:54:49 -0300

    Criação de estrutura inicial do layout

---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <div class="inner-content">
+     <AppLogo />
+     <form>
+      <input type="text">
+      <button type="submit">Buscar</button>
+     </form>
+
+     <div class="actions">
+      <button>Favoritos</button>
+      <button>Carrinho</button>
+     </div>
+
+    </div>
+  </header>
+</template>
+
+<script>
+import AppLogo from './AppLogo.vue';
+export default {
+  components: {
+    AppLogo
+  }
+}
+</script>
+
+<style scoped>
+.inner-content {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
