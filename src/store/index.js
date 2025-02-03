45d34eb80bd7b0f45caaf923b930d65d22fa4027
Fábio Novais <fabio.novais@gmail.com>
2025-02-03 15:09:33 -0300

    Implementação inicial de carregamento de filmes

---
+++ src/store/index.js
@@ -0,0 +1,102 @@
+// store/index.js
+import { createStore } from 'vuex';
+
+const baseUrl = `https://api.themoviedb.org/3`;
+const apiKey = '7b0e2c434489951eb3a12786a20218b8';
+const moviesUrl = `${baseUrl}/movie/top_rated?language=pt-BR&api_key=${apiKey}`;
+const genresUrl = `${baseUrl}/genre/movie/list?language=pt-BR&api_key=${apiKey}`;
+const token = `eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YjBlMmM0MzQ0ODk5NTFlYjNhMTI3ODZhMjAyMThiOCIsIm5iZiI6MTY5ODEwNjkxMi42MDYwMDAyLCJzdWIiOiI2NTM3MGUyMDFmNzQ4YjAxM2ViNGRlNTciLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.MponbgWZP9kc1xoVpC-h1vAxAVm6Kjlh2bvsrb5mC0c`; 
+
+// Cria o store
+const store = createStore({
+  state: {
+    movies: [],
+    genres: [],
+    currentPage: 1,
+    totalPages: 1,
+    totalResults: 0,
+    loading: false,
+    error: null,
+  },
+  mutations: {
+    SET_MOVIES(state, payload) {
+      state.movies = [...state.movies, ...payload.results];
+      state.currentPage = payload.page + 1;
+      state.totalPages = payload.total_pages;
+      state.totalResults = payload.total_results;
+    },
+    SET_GENRES(state, genres) {
+      state.genres = genres;
+    },
+
+    SET_LOADING(state, loading) {
+      state.loading = loading;
+    },
+    SET_ERROR(state, error) {
+      state.error = error;
+    },
+
+  },
+  actions: {
+    async fetchMovies({ commit, state }, reset = false) {
+
+      if(reset) {
+          state.movies = [];
+          state.genres = [];
+          state.currentPage = 1;
+          state.totalPages = 1;
+          state.totalResults = 0;
+      }
+
+      if (state.loading || state.currentPage > state.totalPages) {
+        return;
+      }
+
+      commit('SET_LOADING', true);
+      commit('SET_ERROR', null);
+
+      try {
+        const response = await fetch(
+          `${moviesUrl}&page=${state.currentPage}`
+        );
+
+        const data = await response.json();
+        console.log("data fetched movies", data)
+
+        commit('SET_MOVIES', data); // Atualiza o estado com os dados recebidos
+      } catch (error) {
+        console.error('Erro ao buscar filmes:', error);
+         commit('SET_ERROR', 'Erro ao carregar produtos.');
+      }
+      finally {
+        commit('SET_LOADING', false);
+      }
+    },
+    async fetchGenres({ commit }) {
+      try {
+        const response = await fetch(
+          genresUrl
+        );
+        const data = await response.json();
+        commit('SET_GENRES', data.genres); // Atualiza a lista de gêneros
+      } catch (error) {
+        console.error('Erro ao buscar gêneros:', error);
+      }
+    },
+
+
+  },
+
+
+  getters: {
+    movies: (state) => state.movies,
+    genres: (state) => state.genres,
+    currentPage: (state) => state.currentPage,
+    totalPages: (state) => state.totalPages,
+    totalResults: (state) => state.totalResults,
+       loading: (state) => state.loading,
+    error: (state) => state.error,
+  },
+});
+
+export default store;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,97 @@
 <template>
+  <div class="movies-wrapper">
+    <div class="movie-card" v-for="movie in movies" :key="movie.id">
+      <div class="movie-card__image-wrapper">
+        <img :src="moviePosterBaseUrl+movie.poster_path" :alt="movie.title" loading="lazy" class="movie-card__image">
+        <p>Lançamento: {{ movie.release_date }}</p>
+        <button>Favoritar</button>
+      </div>
+      <div class="movie-card__content">
+      <h2>Nome: {{ movie.title }}</h2>
+      <p>Nota média: {{ movie.vote_average}}</p>
+      <p>Gêneros: {{ getGenreNames(movie.genre_ids)}}</p>
+      <p>Preço: R$17,99</p>
+      <button>Adicionar</button>
+      </div>
+    </div>
 
+    <div v-if="loading">Carregando...</div>
+    <div v-if="error">{{ error }}</div>
+
+  </div>
 </template>
+
+<style langs="scss">
+  .movies-wrapper {
+    display: grid;
+    gap: 24px;
+    grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+  }
+
+  .movie-card {
+    background: red;    
+    border: solid 1px #000;
+  }
+
+  .movie-card__image {
+    width: 100%
+  }
+
+  .movie-card__content {
+    padding: 16px;
+  }
+
+
+</style>
+
+<script>
+import { mapState, mapActions, mapGetters } from 'vuex';
+
+export default {
+  data() {
+    return {
+      moviePosterBaseUrl: 'https://image.tmdb.org/t/p/w342/'
+    }
+  },
+
+  mounted() {
+    this.fetchMovies(this.currentPage);
+    window.addEventListener('scroll', this.handleScroll); 
+  },
+
+  beforeUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+
+  created() {
+    this.$store.dispatch('fetchGenres');
+  },  
+
+
+  computed: {
+    ...mapState(['movies', 'genres', 'currentPage', 'totalPages', 'totalResults']),
+    ...mapGetters(['loading', 'error']),
+  },
+
+  methods: {
+    ...mapActions(['fetchMovies']),
+    getGenreNames(genreIds) {
+
+      return genreIds
+        .map((id) => {
+          const genre = this.genres.find((genre) => genre.id === id);
+          return genre ? genre.name : '';
+        }).join(', ');
+    },
+
+    handleScroll() {
+      const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+      if (scrollTop + clientHeight >= scrollHeight - 100 && !this.loading) {
+        this.fetchMovies();
+      }
+    },
+
+  }
+}
+
+</script>
